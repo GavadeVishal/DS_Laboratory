0a2cd3f557be0a01ad333429e1446b70df49c2b8
Vishal Gavade <120453822+GavadeVishal@users.noreply.github.com>
2024-10-07 15:51:30 +0530

    Practical No:-2 Uploaded on 30-09-2024

---
+++ Practical No-02/ScatterGather.java
@@ -0,0 +1,55 @@
+import mpi.MPI;
+
+public class ScatterGather {
+    public static void main(String args[]) {
+        // Initialize MPI execution environment
+        MPI.Init(args);
+        // Get the id of the process
+        int rank = MPI.COMM_WORLD.Rank();
+        // Total number of processes is stored in size
+        int size = MPI.COMM_WORLD.Size();
+        int root = 0;
+        // Array which will be filled with data by root process
+        int sendbuf[] = null;
+        sendbuf = new int[size];
+        // Creates data to be scattered
+        if (rank == root) {
+            sendbuf[0] = 10;
+            sendbuf[1] = 20;
+            sendbuf[2] = 30;
+            sendbuf[3] = 40;
+            // Print current process number
+            System.out.print("Processor " + rank + " has data: ");
+            for (int i = 0; i < size; i++) {
+                System.out.print(sendbuf[i] + " ");
+            }
+            System.out.println();
+        }
+
+        // Collect data in recvbuf
+        int recvbuf[] = new int[1];
+
+        // Scatter the data
+        MPI.COMM_WORLD.Scatter(sendbuf, 0, 1, MPI.INT, recvbuf, 0, 1, MPI.INT, root);
+
+        System.out.println("Processor " + rank + " has data: " + recvbuf[0]);
+
+        System.out.println("Processor " + rank + " is doubling the data");
+        recvbuf[0] = recvbuf[0] * 2;
+
+        // Gather the data
+        MPI.COMM_WORLD.Gather(recvbuf, 0, 1, MPI.INT, sendbuf, 0, 1, MPI.INT, root);
+
+        // Display the gathered result
+        if (rank == root) {
+            System.out.println("Process 0 has data: ");
+            for (int i = 0; i < size; i++) {
+                System.out.print(sendbuf[i] + " ");
+            }
+            System.out.println();
+        }
+
+        // Terminate MPI execution environment
+        MPI.Finalize();
+    }
+}
